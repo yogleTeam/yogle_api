42c8c4b1be1d234ad6b22cb1f6ea02824afefc10
ljh3259 <ljh3259@naver.com>
2017-08-21 19:01:04 +0900

    first commit

---
+++ src/main/java/com/yogle/YogleApiApplication.java
@@ -0,0 +1,12 @@
+package com.yogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogleApiApplication.class, args);
+	}
+}
